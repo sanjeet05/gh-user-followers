815b68703091f1819ffd4e89ccd459e33d9b7ead
sanjeet <sanjeet@stream.space>
2021-01-31 15:19:24 +0530

    wip

---
+++ build/precache-manifest.a324722f24cc4a295d55ce566eaae8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60517463192a035899f32ee2a83cdc3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c471129b9f5e97fc9ff",
+    "url": "/static/css/main.a4deeeed.chunk.css"
+  },
+  {
+    "revision": "d45f0cad6258f6767440",
+    "url": "/static/js/2.ce015565.chunk.js"
+  },
+  {
+    "revision": "51bd835a23e1b65aabe0b9626fe81c7b",
+    "url": "/static/js/2.ce015565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c471129b9f5e97fc9ff",
+    "url": "/static/js/main.a76f5e43.chunk.js"
+  },
+  {
+    "revision": "281434349c5937096016",
+    "url": "/static/js/runtime-main.cff76287.js"
+  }
+]);

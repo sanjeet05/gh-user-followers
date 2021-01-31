9dd231d8257e76e204f51cc59d25a443a4b4665b
sanjeet <sanjeet@stream.space>
2021-01-31 15:32:00 +0530

    Updates

---
+++ precache-manifest.8e8b06256d12f4e3ecbc970a80dd4710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29923da13bb7922d26258fca8dd71393",
+    "url": "/gh-user-followers/index.html"
+  },
+  {
+    "revision": "7e3ce44d33bf98b03465",
+    "url": "/gh-user-followers/static/css/main.a4deeeed.chunk.css"
+  },
+  {
+    "revision": "d45f0cad6258f6767440",
+    "url": "/gh-user-followers/static/js/2.ce015565.chunk.js"
+  },
+  {
+    "revision": "51bd835a23e1b65aabe0b9626fe81c7b",
+    "url": "/gh-user-followers/static/js/2.ce015565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3ce44d33bf98b03465",
+    "url": "/gh-user-followers/static/js/main.18d900fe.chunk.js"
+  },
+  {
+    "revision": "3ecc6b812e153e69723e",
+    "url": "/gh-user-followers/static/js/runtime-main.f0202f7f.js"
+  }
+]);
